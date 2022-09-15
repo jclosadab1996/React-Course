a2257cd1432fe04a4da610563a0f2736cfd6e20c
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2022-09-16 01:58:30 +0900

    Seccion de javascript culminada

---
+++ 02-intro-javascript/src/bases/01-const.js
@@ -0,0 +1,16 @@
+// Variables y constantes
+
+const nombre = "juan";
+const apellido = "Losada";
+
+let valorDado = 10;
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+  const nombre = "peter";
+
+  console.log(nombre);
+}
+
+console.log(valorDado);
