3a7f045d9e29606dcbf64fa1e38150167f0284bb
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2022-10-01 01:36:33 +0900

    Finzalicion del proyecto

---
+++ 05-GifExpertApp/src/components/AddCategory.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+export const AddCategory = ({ onNewCategory }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim().length <= 1) return;
+
+    // setCategories((categories) => [inputValue, ...categories]);
+    onNewCategory(inputValue.trim());
+    setInputValue("");
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="Palabra Clave"
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
