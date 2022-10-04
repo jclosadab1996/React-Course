7c1088cc0e9eec64472c4ceccfef5bd60c65f10d
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2022-10-04 19:20:46 +0900

    terminado conociumientos basicos

--- 06-hook-app/src/main.jsx
+++ 06-hook-app/src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { MultipleCustomHooks } from "./03-examples";
 import { FocusScreen } from "./04-UseRef/FocusScreen";
+import { Layout } from "./05-useLayoutEffect/Layout";
+import { CallbackHook } from "./06-memos/CallbackHooks";
+import { MemoHook } from "./06-memos/MemoHook";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 
@@ -11,7 +14,7 @@ import { FocusScreen } from "./04-UseRef/FocusScreen";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <FocusScreen />
-  </React.StrictMode>
+  //<React.StrictMode>
+  <CallbackHook />
+  //</React.StrictMode>
 );
